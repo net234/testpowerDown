57210ccf9c4c0b95ef2357de4c8e57561787b6b0
net234 <51489394+net234@users.noreply.github.com>
2021-01-08 20:09:05 +0100

    OK avec reconnaissance interrupted sleep

--- testpowerdown/testpowerdown.ino
+++ testpowerdown/testpowerdown.ino
@@ -19,12 +19,14 @@
 #define LED_1  LED_BUILTIN
 #define LED_ON LOW
 #define LED_OFF (!LED_ON)
-//ENABLE_BP0TORESET (TXD) GPIO2 D4 is used as ENABLE BP0 to reset
-//ENABLE_BP0TORESET (FLASH) GPIO0 D3 is used as ENABLE BP0 to reset
-//ENABLE_BP0TORESET (MTD0) GPIO15 D8 is used as ENABLE BP0 to reset
-//ENABLE_BP0TORESET (WAKE) GPIO16 D0 is used as ENABLE BP0 to reset
-#define LOCK  LOW
-#define ENABLE_BP0TORESET 15
+
+////ENABLE_BP0TORESET (TXD) GPIO2 D4 is used as ENABLE BP0 to reset
+////ENABLE_BP0TORESET (FLASH) GPIO0 D3 is used as ENABLE BP0 to reset
+////ENABLE_BP0TORESET (MTD0) GPIO15 D8 is used as ENABLE BP0 to reset
+////ENABLE_BP0TORESET (WAKE) GPIO16 D0 is used as ENABLE BP0 to reset
+//#define LOCK  LOW
+//#define ENABLE_BP0TORESET 15
+
 //BP_0 (MOSI)   D7   GPIO13 is Used as BP0 status (pullup)
 #define BP_0 13
 
@@ -41,72 +43,108 @@
 // REASON_EXT_SYS_RST      = 6 /* external system reset */
 //};
 
+
+
+
+
+#define REASON_DEEP_SLEEP_AWAKE_ABORTED  ( (rst_reason)10)
+
+
 rst_info* resetInfoPtr;
 
-struct savedMemory_t {
-  char topTxt[16];
-  uint16_t compteur;
-  char bottomTxt[16];
-};
-savedMemory_t savedMemory __attribute__ ((section (".noinit")));
+//struct savedMemory_t {
+//  char topTxt[16];
+//  uint16_t compteur;
+//  char bottomTxt[16];
+//};
+//savedMemory_t savedMemory __attribute__ ((section (".noinit")));
+
+struct savedRTCmemory_t {
+  float     checkPI;   // initialised to PI value to check POWER_ON Boot
+  uint32_t  bootCounter;
+  bool      RepeatPowerDown;
+} savedRTCmemory;
+
+
+
 bool bp0Status;
 void setup() {
-  resetInfoPtr = ESP.getResetInfoPtr();
-
-
-  // init Serial
-  Serial.begin(115200);
-  Serial.println();
-  switch (resetInfoPtr->reason) {
-    case REASON_DEFAULT_RST:  Serial.println(F("->Cold boot")); break;
-    case REASON_EXT_SYS_RST:  Serial.println(F("->boot with BP Reset")); break;
-    case REASON_DEEP_SLEEP_AWAKE:  Serial.println(F("->boot from a deep sleep")); break;
-    default:
-      Serial.print(F("->boot reason = "));
-      Serial.println(resetInfoPtr->reason);
-  }
+  // get BP_0 to know if it is a SLEEP_AWAKE or SLEEP_AWAKE_ABORTED
   pinMode(BP_0, INPUT);
   bp0Status = digitalRead(BP_0);
-  Serial.print(("BP_0="));
-  Serial.println(bp0Status);
-  delay(10);
-  bp0Status = digitalRead(BP_0);
-  Serial.print(("BP_0="));
-  Serial.println(bp0Status);
-
-
+  // get reset reason
+  resetInfoPtr = ESP.getResetInfoPtr();
+  if (bp0Status == LOW && resetInfoPtr->reason == REASON_DEEP_SLEEP_AWAKE) resetInfoPtr->reason = REASON_DEEP_SLEEP_AWAKE_ABORTED;
+  //Arduino/tools/sdk/include/user_interface.h 
+  //     system_rtc_mem_write(10, &compteur, 2); //offset is 65
   Serial.println(F( "\r\n" APP_VERSION ));
+//  if (resetInfoPtr->reason == REASON_DEEP_SLEEP_AWAKE ) {
+////    Serial.println(F("Retourne PowerDown 5 sec"));
+//    ESP.deepSleep(5 * 1E6, RF_DISABLED);
+//  }
 
-  // check saved memory
-  String aString = APP_VERSION;
-  if ( aString.startsWith(savedMemory.topTxt) && aString.startsWith(savedMemory.bottomTxt) ) {
-    Serial.println(F( "saved memory intact" ));
+  
+
+// init Serial
+  Serial.begin(115200);
+  Serial.println();
+  if (resetInfoPtr->reason == REASON_DEEP_SLEEP_AWAKE ) {
+    Serial.println(F("-> PowerDown 10 min"));
+    ESP.deepSleep(10 * 60 * 1E6 - 187000, RF_DISABLED);
+    while (true) delay(1);
+  }
+
+  //system_rtc_mem_read(10, &savedRTCmemory, sizeof(savedRTCmemory));
+  ESP.rtcUserMemoryRead(0,(uint32_t*)&savedRTCmemory, sizeof(savedRTCmemory));
+  if (savedRTCmemory.checkPI != float(PI)) {
+    Serial.println("Power on boot");
+    savedRTCmemory.checkPI = PI;
+    savedRTCmemory.bootCounter = 0;
+    savedRTCmemory.RepeatPowerDown = false;
   } else {
-    // init saved memory
-    Serial.println(F( "Init saved memory" ));
-    strncpy(savedMemory.topTxt, aString.c_str(), 15);
-    savedMemory.topTxt[15] = 0;
-    strncpy(savedMemory.bottomTxt, aString.c_str(), 15);
-    savedMemory.bottomTxt[15] = 0;
-
-    savedMemory.compteur = 0;
+    savedRTCmemory.bootCounter++;
+    if (bp0Status == LOW ) savedRTCmemory.RepeatPowerDown = false;
   }
-  if ( !(aString.startsWith(savedMemory.topTxt) && aString.startsWith(savedMemory.bottomTxt)) ) {
-    // init saved memory
-    Serial.println(F( "Bad Init saved memory" ));
-  }
+  ESP.rtcUserMemoryWrite(0,(uint32_t*)&savedRTCmemory, sizeof(savedRTCmemory));
+  //system_rtc_mem_write(10, &savedRTCmemory, sizeof(savedRTCmemory));
+  Serial.print("savedRTCmmemory.bootCounter = ");
+  Serial.println(savedRTCmemory.bootCounter);
 
 
+  
   switch (resetInfoPtr->reason) {
     case REASON_DEFAULT_RST:  Serial.println(F("->Cold boot")); break;
     case REASON_EXT_SYS_RST:  Serial.println(F("->boot with BP Reset")); break;
     case REASON_DEEP_SLEEP_AWAKE:  Serial.println(F("->boot from a deep sleep")); break;
+    case REASON_DEEP_SLEEP_AWAKE_ABORTED: Serial.println(F("->boot from a deep sleep aborted with BP User")); break;
     default:
       Serial.print(F("->boot reason = "));
       Serial.println(resetInfoPtr->reason);
   }
-  digitalWrite(ENABLE_BP0TORESET, LOCK );
-  pinMode(ENABLE_BP0TORESET, OUTPUT);
+
+  Serial.println(F( "\r\n" APP_VERSION ));
+  if (resetInfoPtr->reason == REASON_DEEP_SLEEP_AWAKE ) {
+    Serial.println(F("Retourne PowerDown 5 sec"));
+    ESP.deepSleep(5 * 1E6, RF_DISABLED);
+  }
+  //  // check saved memory
+  //  String aString = APP_VERSION;
+  //  if ( aString.startsWith(savedMemory.topTxt) && aString.startsWith(savedMemory.bottomTxt) ) {
+  //    Serial.println(F( "saved memory intact" ));
+  //  } else {
+  //    // init saved memory
+  //    Serial.println(F( "Init saved memory" ));
+  //    strncpy(savedMemory.topTxt, aString.c_str(), 15);
+  //    savedMemory.topTxt[15] = 0;
+  //    strncpy(savedMemory.bottomTxt, aString.c_str(), 15);
+  //    savedMemory.bottomTxt[15] = 0;
+  //
+  //    savedMemory.compteur = 0;
+  //  }
+  //  if ( !(aString.startsWith(savedMemory.topTxt) && aString.startsWith(savedMemory.bottomTxt)) ) {
+  //    // init saved memory
+  //    Serial.println(F( "Bad Init saved memory" ));
+  //  }
 
   pinMode( LED_1, OUTPUT );
   //pinMode( LED_2, OUTPUT );
@@ -116,28 +154,33 @@ void setup() {
 
   // Wifi off
   if ( WiFi.getMode() != WIFI_OFF)  {
-    Serial.print(F("WiFiMode="));
+    Serial.print(F("WiFiMode = "));
     Serial.println(WiFi.getMode());
   }
   wifi_fpm_set_sleep_type(LIGHT_SLEEP_T);
   WiFi.forceSleepBegin();  // this do  a WiFiMode OFF  !!! 21ma
+  // return to sleep
+  if(resetInfoPtr->reason == REASON_DEEP_SLEEP_AWAKE) {
+    
+  }
+
 
   bp0Status = digitalRead(BP_0);
-  Serial.print(("BP_0="));
+  Serial.print(("BP_0 = "));
   Serial.println(bp0Status);
 
-
-
   Serial.println(F("Bonjour ..."));
-  Serial.println(resetInfoPtr->reason);
-  if (resetInfoPtr->reason != REASON_DEEP_SLEEP_AWAKE) {
-    savedMemory.compteur = 0;
-  } else {
-    savedMemory.compteur++;
-  }
+  //  Serial.println(resetInfoPtr->reason);
+  //  if (resetInfoPtr->reason != REASON_DEEP_SLEEP_AWAKE) {
+  //    savedMemory.compteur = 0;
+  //  } else {
+  //    savedMemory.compteur++;
+  //  }
   Serial.print(F("compteur = "));
-  Serial.println(savedMemory.compteur);
+  Serial.println(savedRTCmemory.bootCounter);
   Serial.println(F("Type S for DeepSleep"));
+  Serial.print(F("RTC Time="));
+  Serial.println(system_get_rtc_time());
   bp0Status = !digitalRead(BP_0);
 }
 
@@ -145,40 +188,17 @@ void loop() {
   if (Serial.available()) {
     char aChar = (char)Serial.read();
     if (aChar == 'S') {
-
-      //  if (resetInfoPtr->reason == REASON_EXT_SYS_RST || resetInfoPtr->reason == REASON_DEEP_SLEEP_AWAKE) {
-//      Serial.println(F("---->DeepSleep"));
-//      pinMode(ENABLE_BP0TORESET, OUTPUT);
-//      //pinMode(ENABLE_BP0TORESET, INPUT);
-//
-//      digitalWrite(ENABLE_BP0TORESET, !LOCK );
-      Serial.println(F("---->Enable BP0 to reset"));
-      ESP.deepSleep(5E6, RF_DISABLED);
-      //      WiFi.suspend();
-      //      WiFi.resume();
-      int compteur;
-      system_rtc_mem_write(65, &compteur, 2); //offset is 65
-      system_rtc_mem_read(65, &compteur, 2); //offset is 65
-    }
-    if (aChar == 'U') {
-
-      Serial.println(F("---->Unlock"));
-      pinMode(ENABLE_BP0TORESET, OUTPUT);
-      //pinMode(ENABLE_BP0TORESET, INPUT);
-
-      digitalWrite(ENABLE_BP0TORESET, !LOCK );
+      Serial.print(F("PowerDown 5 sec"));
+      ESP.deepSleep(5 * 1E6, RF_DISABLED);
     }
     if (aChar == 'L') {
-      Serial.println(F("---->Lock"));
-      pinMode(ENABLE_BP0TORESET, OUTPUT);
-      //pinMode(ENABLE_BP0TORESET, INPUT);
-
-      digitalWrite(ENABLE_BP0TORESET, LOCK );
+      Serial.print(F("PowerDown 1 Hour"));
+      ESP.deepSleep(60 * 60 * 1E6, RF_DISABLED);
     }
   }
   if ( bp0Status != digitalRead(BP_0) ) {
     bp0Status = !bp0Status;
-    Serial.print(F("BP0="));
+    Serial.print(F("BP0 = "));
     Serial.println(bp0Status);
   }
 
